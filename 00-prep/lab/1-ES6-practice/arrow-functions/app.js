1a0ca023cec871f2de2e0e49da43361b719921e2
Charles Clemens <cclemensjr@gmail.com>
2018-10-15 12:17:59 -0700

    refactored Student into an arrow function for step 10

--- 00-prep/lab/1-ES6-practice/arrow-functions/app.js
+++ 00-prep/lab/1-ES6-practice/arrow-functions/app.js
@@ -133,22 +133,30 @@ let message = name => `Hello, ${name}`;
 console.log(message('Allie'));
 
 
-let Student = function(name, age, hometown) {
-  this.name = name;
-  this.age = age;
-  this.hometown = hometown;
-};
+// let Student = function(name, age, hometown) {
+//   this.name = name;
+//   this.age = age;
+//   this.hometown = hometown;
+// };
 
-let joe = new Student('Joe', 'Schmoe', 100);
 
+let Student = (name, age, hometown) => ({
+  this.name = name,
+  this.age = age,
+  this.hometown = hometown
+});
+
+let joe = Student('Joe', 'Schmoe', 100);
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
 console.log(joe);
 
 
-Student.prototype.greeting = function() {
-  return `Hi, my name is ${this.name}`;
-};
+// Student.prototype.greeting = function() {
+//   return `Hi, my name is ${this.name}`;
+// };
+
+let Student.greeting = () => `Hi, my name is ${this.name}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
