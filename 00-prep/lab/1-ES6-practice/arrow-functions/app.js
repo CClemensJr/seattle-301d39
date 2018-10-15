6155074051694622033060460392e17539b07702
Charles Clemens <cclemensjr@gmail.com>
2018-10-15 12:05:58 -0700

    refactored message into an arrow function for step 10

--- 00-prep/lab/1-ES6-practice/arrow-functions/app.js
+++ 00-prep/lab/1-ES6-practice/arrow-functions/app.js
@@ -127,9 +127,7 @@ let sumAndProduct = (a, b) => {
 console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
+let message = name => `Hello, ${name}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 console.log(message('Allie'));
